ebfbaeec76f8476f8bf4b08a57b271e5fff3c39a
Kir Shatrov <shatrov@me.com>
2023-08-14 06:13:27 +0000

    naming

--- ext/cloudsqlite/extconf.rb
+++ ext/cloudsqlite/extconf.rb
@@ -1,13 +1,10 @@
 require "mkmf"
 
-unless find_header("yaml.h") && find_library("yaml", "yaml_get_version")
-  abort("\nERROR: *** could not find libyaml development environment ***\n\n")
-end
 unless find_header("sqlite3.h") && find_library("sqlite3", "sqlite3_libversion_number", "sqlite3.h")
-  abort "damn"
+  abort "sqlite3 is missing"
 end
 unless find_library("curl", "curl_multi_init")
-  abort "ddd"
+  abort "curl is missing"
 end
 
 create_makefile("cloudsqlite")
